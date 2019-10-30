9ebf31b38bea3b13851beaef1858a3aac32ab681
bradical1379 <bradical1379@users.noreply.github.com>
2019-10-30 08:51:20 -0500

    Update example.py

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from pydfs_lineup_optimizer import Site, Sport, get_optimizer
 
 
 optimizer = get_optimizer(Site.DRAFTKINGS, Sport.BASKETBALL)
-optimizer.load_players_from_csv("https://www.draftkings.com/lineup/getavailableplayerscsv?contestTypeId=70&draftGroupId=30978")
+optimizer.load_players_from_csv("DKSalaries.csv")
 lineup_generator = optimizer.optimize(10)
 for lineup in lineup_generator:
     print(lineup)
